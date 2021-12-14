635154cadde952ede3bd392ecb584b6706494374
Newbie012 <co.eliya2@gmail.com>
2021-12-14 22:30:09 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// @ts-ignore
+import * as M from "react-mapbox-wrapper";
+
+console.log("M", M);
+
+function App() {
+    return <div>Open devtools</div>;
+}
+
+export default App;
